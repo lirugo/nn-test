77a91c08e827f4d6346505e67cd9714bd2e6a9d5
user <user@MacBook-Air-xuser.local>
2020-05-01 12:53:16 +0300

    Init

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello12" << endl;
+    cout << "Hello12" << endl;
+    cout << "Hello12" << endl;
+
+    return 0;
+} 
